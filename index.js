e4b59aaf53062aa9f8c64702fd0515b1f270f604
maryamfarrag <37194683+MaryamFarrag@users.noreply.github.com>
2019-02-03 13:16:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $(".loading").css("z-index", "0");
+    $(".cssload-thecube").css("opacity","0")
+    $(".loading").css("width", "0");
+    $("body").css("overflow", "auto")
+})
